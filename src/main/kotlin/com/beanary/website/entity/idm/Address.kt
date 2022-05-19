cdd11a650345c78570bf572b04b2a646f1b00df9
Yusuf T420 <yoesoff@gmail.com>
2022-05-19 19:30:56 +0700

    Update users and addresses entity

--- src/main/kotlin/com/beanary/website/entity/idm/Address.kt
+++ src/main/kotlin/com/beanary/website/entity/idm/Address.kt
@@ -1,17 +1,24 @@
 package com.beanary.website.entity.idm
 
-import org.springframework.data.annotation.Id
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
+import javax.persistence.*
 
 @Entity
 @Table(name = "addresses")
-class Address(
-    @javax.persistence.Id
-    @Column(name = "id", nullable = false)
-    open var id: Long? = null
+class Address (
 
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    var id: Long? = null,
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    var createdAt: LocalDateTime,
+
+    @Column(name = "updated_at", updatable = false)
+    @UpdateTimestamp
+    var updatedAt: LocalDateTime
 )
--- src/main/kotlin/com/beanary/website/entity/idm/User.kt
+++ src/main/kotlin/com/beanary/website/entity/idm/User.kt
@@ -1,16 +1,33 @@
 package com.beanary.website.entity.idm
 
-import org.springframework.data.annotation.Id
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.LocalDateTime
+import javax.persistence.*
 
 @Entity
 @Table(name = "users")
 class User(
+
     @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    open var id: Long? = null
+    var id: Long? = null,
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    var createdAt: LocalDateTime,
+
+    @Column(name = "updated_at", updatable = false)
+    @UpdateTimestamp
+    var updatedAt: LocalDateTime,
+
+    @Column(name = "firstname", nullable = false)
+    var firstname: String? = null,
+
+    @Column(name = "middlename", nullable = false)
+    var middlename: String? = null,
+
+    @Column(name = "lastname", nullable = false)
+    var lastname: String? = null
 )
